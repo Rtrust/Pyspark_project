2b6210eb6bec3a2600494e05bfd3d8009d6f1b27
Pixel <61188160+Rtrust@users.noreply.github.com>
2023-03-31 14:37:03 +0100

    ex3

--- airflow.py
+++ airflow.py
@@ -34,4 +34,33 @@ default_dag_args = {
      'project_id': 1 }
 
 with DAG("first_python_dag", schedule_interval = '@daily', catchup=False, default_args = default_dag_args) as dag_python:
-task_0 = PythonOperator(task_id = "first_python_task", python_callable = python_first_function)"
+    task_0 = PythonOperator(task_id = "first_python_task", python_callable = python_first_function)
+
+
+#exo3
+import requests 
+import time 
+import json from airflow
+import DAG from airflow.operators.python_operator
+import PythonOperator from airflow.operators.python
+import BranchPythonOperator from airflow.operators.dummy
+import DummyOperator
+from datetime import datetime, timedelta 
+import pandas as pd 
+import numpy as np 
+import os
+
+def get_data(**kwargs):
+    pass
+
+default_dag_args = {
+    'start_date': datetime(2022, 9, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'project_id': 1
+}
+
+with DAG('market_data_dag', default_args=default_dag_args, schedule_interval='@daily') as dag:
+    task_0 = PythonOperator(task_id = "get_market_data", python_callable = get_data, op_kwargs = {'tickers' : []})"
